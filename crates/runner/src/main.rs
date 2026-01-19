370df4cf593dcb263fde8ebad58d8497268319a2
taga3s <bepozurapu274@gmail.com>
2026-01-19 12:30:28 +0900

    refactor(runner): improve codes

--- crates/runner/src/main.rs
+++ crates/runner/src/main.rs
@@ -10,29 +10,31 @@ fn main() {
     }
 
     let file_path = &args[1];
-    // if !file_path.ends_with(".monkey") {
-    //     eprintln!("Error: The file must have a .monkey extension.");
-    //     std::process::exit(1);
-    // }
-    let path = Path::new(file_path);
-    let display = path.display();
-
-    let mut file = match File::open(path) {
-        Err(why) => {
-            eprintln!("couldn't open {}: {}", display, why);
-            std::process::exit(1);
-        }
-        Ok(file) => file,
-    };
-
-    let mut s = String::new();
-    if let Err(err) = file.read_to_string(&mut s) {
-        eprintln!("couldn't read {}: {}", display, err);
+    if !file_path.ends_with(".mon") {
+        eprintln!("Error: The file must have a \".mon\" extension.");
         std::process::exit(1);
     }
 
-    let out = run(&s);
-    if let Some(result) = out {
-        println!("{}", result);
+    let path = Path::new(file_path);
+    let display = path.display();
+    let mut file = match File::open(path) {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!("couldn't open {}: {}", display, err);
+            std::process::exit(1);
+        }
+    };
+
+    let mut buf = String::new();
+    match file.read_to_string(&mut buf) {
+        Ok(_) => {
+            if let Some(result) = run(&buf) {
+                println!("{}", result);
+            }
+        }
+        Err(err) => {
+            eprintln!("couldn't read {}: {}", display, err);
+            std::process::exit(1)
+        }
     }
 }
