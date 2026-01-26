a3a878b8cd530bd635f4ec138770a7db43f1c3f7
taga3s <bepozurapu274@gmail.com>
2026-01-27 00:41:16 +0900

    chore(playground): format code and update configurations for consistency

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  site: "https://taga3s.github.io",
-  base: "/monkey-rs"
+	integrations: [react()],
+	site: "https://taga3s.github.io",
+	base: "/monkey-rs",
 });
